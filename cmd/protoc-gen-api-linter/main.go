a470f98c5296847bfb86833010e015889342c3bc
Jon Bennett <jon.bennett@futurenet.com>
2021-12-12 09:36:24 +0000

    fix: Parse flags to enable version printing; Rename options to more clearly indicate plurality;

--- cmd/protoc-gen-api-linter/main.go
+++ cmd/protoc-gen-api-linter/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/jbendotnet/protoc-gen-api-linter/internal/apilinter"
+
 	"github.com/sgreben/flagvar"
 	"google.golang.org/protobuf/compiler/protogen"
 )
@@ -16,31 +17,39 @@ const (
 	defaultReportFilename = "api_linter.json"
 )
 
+// Goreleaser vars
 var (
 	version = "dev"
 	commit  = "unknown"
 	date    = "unknown"
 )
 
+// cli config
 type config struct {
 	PrintVersion         bool
-	LinterEnableRule     flagvar.StringSet
-	LinterDisableRule    flagvar.StringSet
+	LinterEnabledRules     flagvar.StringSet
+	LinterDisabledRules    flagvar.StringSet
 	PluginReportFilename string
 	ReportPrettyPrint    bool
 }
-
 var cfg config
 
 func main() {
 	// set logger to pint to stderr
 	log.SetOutput(os.Stderr)
+	// parse config options via CLI flags
 	fs := flag.NewFlagSet(appName, flag.ExitOnError)
 	fs.BoolVar(&cfg.PrintVersion, "version", false, "Print version and exit.")
-	fs.Var(&cfg.LinterEnableRule, "enable_rule", "Enable a rule with the given name.\nMay be specified multiple times.")
-	fs.Var(&cfg.LinterDisableRule, "disable_rule", "Disable a rule with the given name.\nMay be specified multiple times.")
+	fs.Var(&cfg.LinterEnabledRules, "enable_rule", "Enable a rule with the given name.\nMay be specified multiple times.")
+	fs.Var(&cfg.LinterDisabledRules, "disable_rule", "Disable a rule with the given name.\nMay be specified multiple times.")
 	fs.StringVar(&cfg.PluginReportFilename, "report_filename", defaultReportFilename, "Disable a rule with the given name.\nMay be specified multiple times.")
 	fs.BoolVar(&cfg.ReportPrettyPrint, "report_pretty_print", false, "Pretty print JSON reports")
+	if err := fs.Parse(os.Args[1:]); err != nil {
+		fs.Usage()
+		fmt.Printf("invalid args, err: %s", err)
+		os.Exit(1)
+	}
+
 	if cfg.PrintVersion {
 		fmt.Printf("Version %v, commit %v, built at %v\n", version, commit, date)
 		os.Exit(0)
@@ -54,8 +63,8 @@ func main() {
 func runPlugin(gen *protogen.Plugin) error {
 	opts := apilinter.PluginOptions{
 		Linter: apilinter.LinterOptions{
-			EnabledRules:  cfg.LinterEnableRule.Values(),
-			DisabledRules: cfg.LinterDisableRule.Values(),
+			EnabledRules:  cfg.LinterEnabledRules.Values(),
+			DisabledRules: cfg.LinterDisabledRules.Values(),
 		},
 		ReportFilename:    cfg.PluginReportFilename,
 		ReportPrettyPrint: cfg.ReportPrettyPrint,
